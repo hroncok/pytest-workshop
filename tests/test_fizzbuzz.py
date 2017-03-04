37afddccc1490a6079bda1b4701cb0eb00e32e30
Miro Hrončok <miro@hroncok.cz>
2017-03-04 15:48:57 +0100

    Next step

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,10 +1,24 @@
+import pytest
+
 from fizzbuzz import fizzbuzz
+
+@pytest.fixture(params=(5, 25, 50, 70))
+def numbuzz(request):
+    return request.param
+
+#@pytest.fixture(params=('sqlite', 'postgres')
+#def db(request):
+#    if request.param == 'sqlite':
+
 
 def test_fizz():
     assert fizzbuzz(3) == 'fizz'
 
-def test_buzz():
-    assert fizzbuzz(5) == 'buzz'
+def test_buzz(numbuzz):
+    assert fizzbuzz(numbuzz) == 'buzz'
+
+def test_str_buzz(numbuzz):
+    assert str(fizzbuzz(numbuzz)) == 'buzz'
 
 def test_fizzbuzz():
     assert fizzbuzz(15) == 'fizzbuzz'
