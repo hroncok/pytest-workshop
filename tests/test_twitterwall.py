22a3f223b400d8a2f17abbea06b8cda218d0c97a
Miro Hrončok <miro@hroncok.cz>
2017-03-04 16:40:24 +0100

    Next step

--- tests/test_twitterwall.py
+++ tests/test_twitterwall.py
@@ -1,6 +1,6 @@
 import pytest
 
-from twitterwall import format
+from twitterwall import *
 
 
 @pytest.fixture(scope='module', params=('foo', 'bar'))
@@ -19,3 +19,12 @@ def status(request):
 
 def test_format_includes_author(status):
     assert status['user']['screen_name'] in format(status)
+
+
+
+@pytest.mark.parametrize('term', ('installfest', 'linuxdays'))
+def test_search_results_include_term(term):
+    key, secret = credentials()
+    session = twitter_session(key, secret)
+    results = search(session, term)
+    assert len(results) > 5
